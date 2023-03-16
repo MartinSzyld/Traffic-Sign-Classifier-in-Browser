b28777d187d34d45b95226fa0d447881d483d078
mszyld <mszyld@gmail.com>
2023-03-16 13:14:08 -0300

    code cleanup

--- script-traffic.js
+++ script-traffic.js
@@ -7,25 +7,16 @@ var pos = {x:0, y:0};
 var rawImage;
 var model;
 var color = 'red';
+
 const CATEGORIES = 
 ["Red Speed 20", "Red Speed 30", "Red Speed 50", "Red Speed 60", "Red Speed 70", "Red Speed 80", "Gray Speed 80", "Red Speed 100", "Red Speed 120", 
 	"Red Two Cars", "Red Truck and Car", "Blackie", "Yellow Danger", "Yield", "Stop", "Red Circle", "Red Truck", "Wrong Way", 
 	"Red Danger (!)", "Red Danger Left", "Red Danger Right", "Red Danger Zigzag", "Red Danger Pothole", "Red Danger Slip", "Red Danger Narrow", "Red Danger Working", " danger lights", 
 	"Red Danger Walking", "Red Danger Kids", "Red Danger Bike", "Red Danger Snow", "Red Danger Deer", "Gray Ending", "Blue Right", "Blue Left", "Blue Straight", 
 	"Blue Straight Right", "Blue Straight Left", "Blue Right Down", "Blue Left Down", "Blue Roundabout", "Gray Two Cars", "Gray Truck and Car"];
-
-
-// ===================
-// == Configuration ==
-// ===================
 const lineWidth = 10;
-const halfLineWidth = lineWidth / 2;
-// const fillStyle = '#333';
-// const strokeStyle = '#333';
 const shadowColor = '#333';
 const shadowBlur = lineWidth / 4;
-
-
 
 // copy a crop of bigImage to the canvas, depending on cursor position at event e
 function crop_copy_image(e) {
@@ -47,7 +38,7 @@ window.addEventListener('load', function() {
 			img.src = URL.createObjectURL(this.files[0]); // set src to blob url
 		}
 	});
-  });
+});
 
 // copy the uploaded image to the canvas
 function copy_uploaded_image() 
@@ -58,12 +49,9 @@ function copy_uploaded_image()
 }
 
 // functions for drawing in canvas, adapted from
-// src: https://www.coursera.org/learn/browser-based-models-tensorflow/home/week/2
-// ====================
-// == Event Handlers ==
-// ====================
+// "HTML Canvas Drawing with Touch Support" @ CodePen
 function handleWritingStart(event) {
-	
+	event.preventDefault();
 	const radioButtons = document.querySelectorAll('input[name="color_radio"]');
 	for (const radioButton of radioButtons) {
 		if (radioButton.checked) {
@@ -71,86 +59,43 @@ function handleWritingStart(event) {
 			break;
 		}
 	}
-
-	event.preventDefault();
-  
-	const mousePos = getMosuePositionOnCanvas(event);
+	const mousePos = getMousePositionOnCanvas(event);
 	
 	ctx.beginPath();
-  
-	ctx.moveTo(mousePos.x, mousePos.y);
-  
+	ctx.moveTo(mousePos.x, mousePos.y); 
 	ctx.lineWidth = lineWidth;
-	ctx.strokeStyle = color;
-	ctx.shadowColor = null;
-	ctx.shadowBlur = null;
-  
+	ctx.strokeStyle = color; 
 	ctx.fill();
-	
 	state.mousedown = true;
-  }
-  
-  function handleWritingInProgress(event) {
+}
+
+function handleWritingInProgress(event) {
 	event.preventDefault();
-	
 	if (state.mousedown) {
-	  const mousePos = getMosuePositionOnCanvas(event);
-  
+	  const mousePos = getMousePositionOnCanvas(event);
 	  ctx.lineTo(mousePos.x, mousePos.y);
 	  ctx.stroke();
 	}
-  }
+}
   
-  function handleDrawingEnd(event) {
+function handleDrawingEnd(event) {
 	event.preventDefault();
-	
 	if (state.mousedown) {
-	  ctx.shadowColor = shadowColor;
-	  ctx.shadowBlur = shadowBlur;
-  
 	  ctx.stroke();
 	}
-	
 	state.mousedown = false;
-  }
+	rawImage.src = canvas.toDataURL('image/png');
+}
 
-  function getMosuePositionOnCanvas(e) {
+function getMousePositionOnCanvas(e) {
 	var rect = canvas.getBoundingClientRect();
 	const clientX = e.clientX || e.touches[0].clientX;
 	const clientY = e.clientY || e.touches[0].clientY;
 	pos.x = clientX - rect.left;
 	pos.y = clientY - rect.top;
-	// const { offsetLeft, offsetTop } = event.target;
-	// const canvasX = clientX - offsetLeft;
-	// const canvasY = clientY - offsetTop;
-  
 	return { x: pos.x, y: pos.y };
-  }
+}
 
-// function setPosition(e){
-// 	var rect = canvas.getBoundingClientRect();
-// 	pos.x = e.clientX - rect.left;
-// 	pos.y = e.clientY - rect.top;
-// }
-// function draw(e) {
-// 	if(e.buttons!=1) return;
-// 	ctx.beginPath();
-// 	ctx.lineWidth = 12;
-// 	ctx.lineCap = 'round';
-// 	const radioButtons = document.querySelectorAll('input[name="color_radio"]');
-// 	for (const radioButton of radioButtons) {
-// 		if (radioButton.checked) {
-// 			color = radioButton.value;
-// 			break;
-// 		}
-// 	}
-// 	ctx.strokeStyle = color;
-// 	ctx.moveTo(pos.x, pos.y);
-// 	setPosition(e);
-// 	ctx.lineTo(pos.x, pos.y);
-// 	ctx.stroke();
-// 	rawImage.src = canvas.toDataURL('image/png');
-// }
 function clear() {
 	ctx.fillStyle = "white";
 	ctx.fillRect(0,0,280,280);
@@ -173,11 +118,18 @@ function classify() {
 
 function init_script() {
 	canvas = document.getElementById('canvas');
-	rawImage = new Image();
 	ctx = canvas.getContext("2d");
-	ctx.fillStyle = "white";
-	ctx.fillRect(0,0,280,280);
-
+	rawImage = new Image();
+	
+	classifyButton = document.getElementById('classify-button');
+	clearButton = document.getElementById('clear-button');
+	bigImage = document.getElementById('img-43-classes');
+	copyImageButton = document.getElementById('copy_image');
+	
+	// Initialize canvas with white pixel from image
+	// this avoids error message when trying to classify the empty canvas
+	ctx.drawImage(bigImage, 0, 450, 1, 1, 0, 0, 280, 280);
+	rawImage.src = canvas.toDataURL('image/png');
 
 	canvas.addEventListener('mousedown', handleWritingStart);
 	canvas.addEventListener('mousemove', handleWritingInProgress);
@@ -188,22 +140,9 @@ function init_script() {
 	canvas.addEventListener('touchmove', handleWritingInProgress);
 	canvas.addEventListener('touchend', handleDrawingEnd);
 
-	// canvas.addEventListener("mousemove", draw);
-	// canvas.addEventListener("mousedown", setPosition);
-	// canvas.addEventListener("mouseenter", setPosition);
-
-	
-	// canvas.addEventListener('touchstart', setPosition);
-	// canvas.addEventListener('touchmove', draw);
-	// canvas.addEventListener('touchend', draw);
-
-	classifyButton = document.getElementById('classify-button');
 	classifyButton.addEventListener("click", classify);
-	clearButton = document.getElementById('clear-button');
 	clearButton.addEventListener("click", clear);
-	bigImage = document.getElementById('img-43-classes');
 	bigImage.addEventListener("click", crop_copy_image);
-	copyImageButton = document.getElementById('copy_image');
 	copyImageButton.addEventListener("click", copy_uploaded_image);
 }
 
@@ -213,44 +152,3 @@ async function load_model() {
 }
 
 document.addEventListener('DOMContentLoaded', load_model);
-
-
-
-    
-
-
-// =============
-// == Globals ==
-// =============
-
-// ===================
-// == Configuration ==
-// ===================
-// const lineWidth = 20;
-// const halfLineWidth = lineWidth / 2;
-// const fillStyle = '#333';
-// const strokeStyle = '#333';
-// const shadowColor = '#333';
-// const shadowBlur = lineWidth / 4;
-
-// =====================
-// == Event Listeners ==
-// =====================
-// clearButton.addEventListener('click', handleClearButtonClick);
-
-
-
-// function handleClearButtonClick(event) {
-//   event.preventDefault();
-  
-//   clearCanvas();
-// }
-
-// ======================
-// == Helper Functions ==
-// ======================
-
-
-// function clearCanvas() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-// }
